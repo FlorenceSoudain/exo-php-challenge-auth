9dbde9c19a6e2b54b0b5596d5bae20e2eb78d74d
FlorenceSoudain <43751399+FlorenceSoudain@users.noreply.github.com>
2019-01-16 16:00:16 +0100

    V1

--- login.php
+++ login.php
@@ -1,24 +1,63 @@
+<?php
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "reunion_island";
+
+$connection = new mysqli($servername, $username, $password);
+
+if ($connection->connect_error) {
+    die("connection failed : " . $connection->connect_error);
+} else {
+    $connection->select_db($dbname);
+}
+
+
+
+$sql = "SELECT username, password FROM user WHERE 1";
+$result = $connection->query($sql);
+while ($row = $result->fetch_assoc()) {
+    $DBusername = $row['username'];
+    $DBpassword = $row['password'];
+}
+
+global $username, $password;
+
+if (isset($_POST['username']) && isset($_POST['password'])){
+if ($DBusername == $_POST['username'] && $DBpassword == $_POST['password']) {
+    session_start();
+
+    $_SESSION['username'] = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+    $_SESSION['password'] = filter_var(($_SESSION['password']), FILTER_SANITIZE_STRING);
+
+    header('Location: read.php');
+}
+}
+
+?>
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
     <title>Login</title>
     <link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
-  </head>
-  <body>
+</head>
+<body>
 
-    <form action="" method="post">
-      <div>
+<form action="" method="post">
+    <div>
         <label for="username">Identifiant</label>
         <input type="text" name="username">
-      </div>
-      <div>
+    </div>
+    <div>
         <label for="password">Mot de passe </label>
         <input type="password" name="password">
-      </div>
-      <div>
-        <button type="button" name="button">Se connecter</button>
-      </div>
-    </form>
-  </body>
+    </div>
+    <div>
+        <input type="submit" name="button" value="Se connecter">
+    </div>
+</form>
+</body>
 </html>
